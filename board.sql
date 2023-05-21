3ac1724fdbdef39147a4516df25bc8b014847280
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-05-21 20:32:46 +0900

    20230521_#1 rest 복습중

--- board.sql
+++ board.sql
@@ -1,1 +1,48 @@
 SELECT *FROM tbl_board tb ;
+SELECT *FROM tbl_member tm ;
+SELECT *FROM tbl_reply tr ;
+
+
+UPDATE tbl_member 
+set auth = 'ADMIN'
+WHERE account = 'admin';
+
+
+ALTER table tbl_member
+add profile_image VARCHAR(200);
+
+
+alter table tbl_reply 
+add account VARCHAR(50) not null;
+
+update tbl_reply 
+set account = 'abcd';
+
+DELETE FROM tbl_board 
+WHERE board_no = '605';
+
+UPDATE tbl_member 
+SET name = '김이이이'
+WHERE account ='yesprofile';
+
+select A.reply_no, A.reply_text,
+	A.account, B.name , B.profile_image 
+from tbl_reply A
+left join tbl_member B 
+on A.account = B.account 
+where board_no = 606;
+
+
+
+alter table tbl_member 
+add login_method VARCHAR(20);
+
+
+update tbl_member 
+set profile_image = concat('/local', profile_image)
+where profile_image is not null 
+   and login_method is null;
+
+
+
+
